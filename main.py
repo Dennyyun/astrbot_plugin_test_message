15878f2a9f00e5645c853234294552e1c5506917
Dennyyun <2194028294@qq.com>
2025-06-21 17:38:42 +0800

    添加群聊天消息处理测试案例4

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class MyPlugin(Star):
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
     @filter.command("helloworld")
     async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令"""  # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        """这是一个 群消息监听插件 test"""  # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         user_name = event.get_sender_name()
         user_id = event.get_sender_id()  # 获取用户 ID
         message_str = event.message_str  # 用户发的纯文本消息字符串
