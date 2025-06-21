0bb89cf6bd73c7e2043b47c7ad57dfcaaf042bcf
Dennyyun <2194028294@qq.com>
2025-06-21 15:59:37 +0800

    test private message

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
-import astrbot.api.message_components  as Comp
+import astrbot.api.message_components as Comp
 from astrbot.core.platform import AstrBotMessage
 
 
@@ -13,24 +13,22 @@ class MyPlugin(Star):
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
+    async def terminate(self):
+        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
     @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent,message:AstrBotMessage):
+    async def helloworld(self, event: AstrMessageEvent, message: AstrBotMessage):
         """这是一个 hello world 指令"""  # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         user_name = event.get_sender_name()
         user_id = event.get_sender_id()  # 获取用户 ID
         message_str = event.message_str  # 用户发的纯文本消息字符串
         message_chain = event.get_messages()  # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_id}, 你发了 {message_str},{message_chain}!")  # 发送一条纯文本消息
+        yield event.plain_result(f"Hello, {user_id}, 你发了 {message_str}!")  # 发送一条纯文本消息
 
-    #     Hello, {Comp.At(qq=user_id)}, 你发了 {message_str}!
 
-    @filter.event_message_type(filter.EventMessageType.GROUP_MESSAGE)
+    @filter.event_message_type(filter.EventMessageType.PRIVATE_MESSAGE)
     async def on_private_message(self, event: AstrMessageEvent):
         message_str = event.message_str
         yield event.plain_result(f"Hello, {message_str}")
-
-
-    async def terminate(self):
-        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
