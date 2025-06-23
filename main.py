d79193c919682e0d17ba3afad7d85aec1202de1d
Dennyyun <2194028294@qq.com>
2025-06-23 23:48:08 +0800

    Revert "测试搜索"
    This reverts commit acef24513282e842d6f4a1b686f350cb6e90c79d.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 import astrbot.api.message_components as Comp
-import python_script
+
 
 @register("my_group_plugin", "Denny", "一个群消息处理插件", "1.2.0")
 class MyPlugin(Star):
@@ -17,20 +17,31 @@ class MyPlugin(Star):
 
     @filter.event_message_type(filter.EventMessageType.GROUP_MESSAGE)
     async def on_group_message(self, event: AstrMessageEvent):
-        """群消息监听回复 """
+        """群消息监听"""
         logger.info(f"收到事件类型: {type(event)}, 消息: {event.message_str}")
-
+        """群消息监听回复"""
         group_id = event.get_group_id()  # 群号
         sender_name = event.get_sender_name()  # 发送者名字
-        user_id = event.get_group_id()  # 发送者qq号
+        user_id = event.get_group_id()# 发送者qq号
         message = event.get_message_str()  # 消息内容
         logger.info(f"收到群{group_id}，{sender_name}的消息：{message}")
+        chain = [
+            Comp.Plain("你好呀！收到你的消息了！"),
+            Comp.At(qq=event.get_sender_id())
+        ]
         if '你好' in message:
-            yield event.chain_result([
-                Comp.Plain("你好呀！收到你的消息了！"), Comp.At(qq=user_id)])
-        elif "淘宝搜索："in message:
-            const_key = message.split("淘宝搜索：")[1]
-            result = python_script.taobao.main(const_key)
-            yield event.chain_result([Comp.Plain(result), Comp.At(qq=user_id)])
+            yield event.chain_result(chain)
         else:
-            yield event.chain_result([Comp.Plain("哈哈哈"), Comp.At(qq=user_id)])
+            yield event.chain_result([Comp.Plain("哈哈哈"), Comp.At(qq=event.get_sender_id())])
+
+
+    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
+    @filter.command("helloworld")
+    async def helloworld(self, event: AstrMessageEvent):
+        """/helloword 指令回复"""  # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        user_name = event.get_sender_name()
+        user_id = event.get_sender_id()  # 获取用户 ID
+        message_str = event.message_str  # 用户发的纯文本消息字符串
+        message_chain = event.get_messages()  # 用户所发的消息的消息链 # from astrbot.api.message_components import *
+        logger.info(message_chain)
+        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!哈哈哈哈@{user_name}")  # 发送一条纯文本消息
